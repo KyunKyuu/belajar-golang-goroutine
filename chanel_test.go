9a7af3be16e7748e466cb5afc7d9cfb82af36204
KyunKyuu <teguh.iqbal782@gmail.com>
2022-06-17 19:05:19 +0700

    RWMutex

--- chanel_test.go
+++ chanel_test.go
@@ -124,3 +124,37 @@ func TestMutex(t *testing.T){
 	time.Sleep(5 * time.Second)
 	fmt.Println("Counter = ", x)
 }
+
+type BankAccount struct {
+	RWMutex sync.RWMutex
+	balance int
+}
+
+func (account *BankAccount)AddBalance(value int) {
+	account.RWMutex.RLock()
+	account.balance += value
+	account.RWMutex.RUnlock()
+}
+
+func (account *BankAccount)GetBalance() int{
+	account.RWMutex.Lock()
+	balance := account.balance
+	account.RWMutex.Unlock()
+	return balance
+}
+
+func TestRWMutex(t *testing.T){
+	account := BankAccount{}
+
+	for i:=0; i<100; i++{
+		go func(){
+			for j := 0; j < 100; j++ {
+				account.AddBalance(1)
+				fmt.Println(account.GetBalance())
+			}
+		}()
+	}
+
+	time.Sleep(5 * time.Second)
+	fmt.Println("Total Balance", account.GetBalance())
+}
