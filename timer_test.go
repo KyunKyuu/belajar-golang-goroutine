c774f7c718571ec04452466748f594a84660769b
KyunKyuu <teguh.iqbal782@gmail.com>
2022-07-22 23:29:55 +0700

    timer

---
+++ timer_test.go
@@ -0,0 +1,29 @@
+package belajar_golang_goroutine
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestTimer(t *testing.T) {
+	timer := time.NewTimer(5 * time.Second)
+	fmt.Println(time.Now())
+
+	time := <- timer.C
+	fmt.Println(time)
+
+}
+
+func TestTimerFunc(t *testing.T){
+	group := sync.WaitGroup{}
+	group.Add(1)
+	time.AfterFunc(5 *time.Second, func() {
+		fmt.Println(time.Now())
+		group.Done()
+	})
+
+	fmt.Println(time.Now())
+	group.Wait()
+}
